874a1cb4d0990a6831e751f5402c38a0eb1daa59
Roshan Shrestha <shrestharoshan776@gmail.com>
2023-12-07 12:33:16 -0500

    reformated scraper file to make class, removed graph generation to display topics only

--- scraper/github_scraper.py
+++ scraper/github_scraper.py
@@ -1,20 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
+class GithubScraper:
+    def __init__(self):
+        self.url = "https://github.com/trending"
+        self.soup = None
+        self.response = None
 
-def send_query():
-    """Send a search query to GitHub trending and return the response."""
-    url = "https://github.com/trending"
-    response = requests.get(url)
-    return response
+    def send_query(self):
+        """Send a search query to GitHub trending and store the response."""
+        self.response = requests.get(self.url)
+        return self.response
+
+    def parse_html(self):
+        """Parse the HTML response using BeautifulSoup."""
+        soup = BeautifulSoup(self.response.text, 'html.parser')
+        self.soup = soup
+        return soup
+
+    def extract_topics(self):
+        """Extract programming language topics from the parsed HTML."""
+        self.parse_html()  # Make sure to call parse_html before extracting topics
+        topic_spans = self.soup.find_all('span', {'itemprop': 'programmingLanguage'})
+        topics = [topic_span.text.strip() for topic_span in topic_spans]
+        return topics
 
 
-def parse_html(response):
-    soup = BeautifulSoup(response.text, 'html.parser')
-    return soup
-
-
-def extract_topics(soup):
-    topic_spans = soup.find_all('span', {'itemprop': 'programmingLanguage'})
-    topics = [topic_span.text.strip() for topic_span in topic_spans]
-    return topics
--- web/app.py
+++ web/app.py
@@ -1,10 +1,13 @@
 # web/app.py
+import sys
+import os
 from flask import Flask, render_template
-from io import BytesIO
 import matplotlib
-import matplotlib.pyplot as plt
 from collections import Counter
-from scraper import github_scraper  # Import the scraper module
+from scraper.github_scraper import GithubScraper
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(project_root)
 
 matplotlib.use('agg')
 app = Flask(__name__)
@@ -12,40 +15,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    github_scraper = GithubScraper()
     # Fetch data using the scraper module
     response = github_scraper.send_query()
 
     if response.status_code == 200:
-        soup = github_scraper.parse_html(response)
-        topic_list = github_scraper.extract_topics(soup)
+        # Example usage:
+        github_scraper.parse_html()
+        topics = github_scraper.extract_topics()
 
-        print(topic_list)
-
-        # Count the occurrences of each programming language
-        language_counts = Counter(topic_list)
-
-        # Extract languages and their counts for plotting
-        languages = list(language_counts.keys())
-        counts = list(language_counts.values())
-
-        # Plotting the bar chart
-        plt.bar(languages, counts, color='skyblue')
-        plt.xlabel('Programming Languages')
-        plt.ylabel('Number of Occurrences')
-        plt.title('Programming Language Distribution')
-        # Rotate x-axis labels for better readability
-        plt.xticks(rotation=45, ha='right')
-        plt.tight_layout()
-
-        # Save the plot to a BytesIO object
-        img_bytes = BytesIO()
-        plt.savefig(img_bytes)
-        img_bytes.seek(0)
-        data = img_bytes.read()
-        print(data)
-
-        # Pass the BytesIO object to the template
-        return render_template('index.html', img_data=data)
+        return render_template('index.html', topics=topics)
 
     else:
         print(f"Failed to retrieve search results. Status code: {response.status_code}")
