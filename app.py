16b437cc43a5b7936c9d948bfc8ff42704a2dad4
Mike <fuchun297@gmail.com>
2023-12-09 22:53:36 -0500

    add button for pieChart+boxPlot and go back to index
    # Conflicts:
    #	app.py

--- app.py
+++ app.py
@@ -1,39 +1,73 @@
-# web/app.py
+# Import necessary libraries and modules
 import base64
 import io
+import os
 import sqlite3
 import sys
-import os
-
-from flask import Flask, render_template, request, redirect, url_for
+import seaborn as sns
 import matplotlib
-from collections import Counter
-
+from flask import Flask, render_template
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-
+import pandas as pd
+from models.repository_model import RepositoryModel
 from scraper.github_scraper import GithubScraper
 
+# Add the project root to the system path
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ''))
 sys.path.append(project_root)
 
+# Set up Flask application
+app = Flask(__name__)
+
+# Configure Matplotlib for Agg backend
 matplotlib.use('agg')
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    duration = request.args.get('duration', '')
+def perform_web_scraping_and_insert():
+    """
+    Perform web scraping using GithubScraper and insert data into the database.
+    """
     github_scraper = GithubScraper()
-    response = github_scraper.fetch_data(duration)
+    response = github_scraper.fetch_data()
 
     if response.status_code == 200:
         repositories = github_scraper.extract_repositories()
-        return render_template('index.html', repoList=repositories)
-    else:
-        error_message = f"Failed to retrieve search results. Status code: {response.status_code}"
-        print(error_message)
-        return render_template('error.html', message=error_message)
+
+        # Connect to the database
+        con = sqlite3.connect("github_trending.db")
+        cur = con.cursor()
+
+        # Drop the table if it exists (for demonstration purposes)
+        cur.execute("DROP TABLE IF EXISTS repositories")
+
+        # Create the 'repositories' table
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS repositories (
+                name TEXT,
+                owner TEXT,
+                description TEXT,
+                language TEXT,
+                stars_total TEXT,
+                stars_today TEXT
+            )
+        """)
+
+        # Insert data into the 'repositories' table
+        cur.executemany("""
+            INSERT INTO repositories VALUES (?, ?, ?, ?, ?, ?)
+        """, [
+            (repo.name, repo.owner, repo.description,
+             repo.language, repo.stars_total, repo.stars_today)
+            for repo in repositories
+        ])
+
+        # Commit the changes to the database
+        con.commit()
+
+        # Close the database connection
+        con.close()
 
 @app.route('/getSomethingFromDB')
 def getSomethingFromDB():
@@ -54,57 +88,147 @@ def getSomethingFromDB():
         movies.append(movie)
     return render_template('get_data.html', movies=movies)
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # Check if the database file exists
+    db_file = "github_trending.db"
+    if not os.path.exists(db_file):
+        # If the database file doesn't exist, create it and perform web scraping
+        perform_web_scraping_and_insert()
+
+    # Connect to the database
+    con = sqlite3.connect(db_file)
+    cur = con.cursor()
+
+    # Check if the 'repositories' table exists and has data
+    cur.execute("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='repositories'")
+    table_exists = cur.fetchone()[0] > 0
+
+    if not table_exists:
+        # If the 'repositories' table doesn't exist or has no data, perform web scraping
+        perform_web_scraping_and_insert()
+
+    # Fetch data from the database
+    cur.execute("SELECT * FROM repositories")
+    repositories_data = cur.fetchall()
+
+    # Close the database connection
+    con.close()
+    print(f'Data from local\n{repositories_data}')
+
+    # Convert data to RepositoryModel instances
+    repositories = [RepositoryModel(*repo_data) for repo_data in repositories_data]
+
+    return render_template('index.html', repoList=repositories)
+
+
 
 @app.route('/graph', methods=['GET', 'POST'])
 def graph():
-    duration = request.args.get('duration', '')
-    github_scraper = GithubScraper()
-    response = github_scraper.fetch_data(duration)
+    # Fetch data from the database
+    con = sqlite3.connect("github_trending.db")
+    cur = con.cursor()
+    cur.execute("SELECT language, COUNT(*) FROM repositories GROUP BY language")
+    data = cur.fetchall()
+    con.close()
 
-    if response.status_code == 200:
-        repositories = github_scraper.extract_repositories()
+    # Create a Matplotlib figure
+    fig = plt.figure(figsize=(10, 6))
 
-        language_list = [repo.language for repo in repositories]
-        # Create a Matplotlib figure
-        fig = plt.figure(figsize=(10, 6))  # Adjust the size as needed
+    # Add a subplot to the figure
+    axis = fig.add_subplot(1, 1, 1)
 
-        # Add a subplot to the figure
-        axis = fig.add_subplot(1, 1, 1)
+    # Extract languages and their counts for plotting
+    languages, counts = zip(*data)
 
-        # Count the occurrences of each programming language
-        language_counts = Counter(language_list)
+    # Plotting the bar chart
+    axis.bar(languages, counts, color='red')
+    axis.set_xlabel('Programming Languages')
+    axis.set_ylabel('Number of Repositories')
+    axis.set_title('Programming Language Distribution')
 
-        # Extract languages and their counts for plotting
-        languages = list(language_counts.keys())
-        counts = list(language_counts.values())
+    # Rotate x-axis labels for better readability
+    axis.set_xticks(languages)
+    axis.set_xticklabels(languages, rotation='vertical')
 
-        # Plotting the bar chart
-        axis.bar(languages, counts, color='skyblue')
-        axis.set_xlabel('Programming Languages')
-        axis.set_ylabel('Number of Occurrences')
-        axis.set_title('Programming Language Distribution')
-        # Rotate x-axis labels for better readability
-        axis.set_xticks(languages)
-        axis.set_xticklabels(languages, rotation='vertical')
-        # Show the plot
-        plt.show()
-        # Convert plot to PNG image
-        pngImage = io.BytesIO()
-        canvas = FigureCanvas(fig)
-        canvas.print_png(pngImage)
+    # Save the plot to a BytesIO object
+    png_image = io.BytesIO()
+    canvas = FigureCanvas(fig)
+    canvas.print_png(png_image)
 
-        # Encode PNG image to base64 string
-        pngImageB64String = "data:image/png;base64,"
-        pngImageB64String += base64.b64encode(
-            pngImage.getvalue()).decode('utf8')
+    # Encode PNG image to base64 string
+    png_image_64 = "data:image/png;base64,"
+    png_image_64 += base64.b64encode(png_image.getvalue()).decode('utf8')
 
-        # Pass the BytesIO object to the template
-        return render_template('graph.html', image=pngImageB64String)
+    # Pass the BytesIO object to the template
+    return render_template('graph.html', image=png_image_64)
 
-    else:
-        error_message = f"Failed to retrieve search results. Status code: {response.status_code}"
-        print(error_message)
-        return render_template('error.html', message=error_message)
+
+@app.route('/pieChart', methods=['GET', 'POST'])
+def pieChart():
+    # Fetch data from the database
+    con = sqlite3.connect("github_trending.db")
+    cur = con.cursor()
+    cur.execute("SELECT language, COUNT(*) FROM repositories GROUP BY language")
+    data = cur.fetchall()
+    con.close()
+
+    # Create a Matplotlib figure
+    fig = plt.figure(figsize=(10, 6))
+
+    # Extract languages and their counts for plotting
+    languages, counts = zip(*data)
+
+    # Plotting the bar chart
+    plt.pie(counts, labels=languages, autopct='%1.1f%%', startangle=90)
+    plt.title('Language Share')
+    plt.show()
+
+    # Save the plot to a BytesIO object
+    png_image = io.BytesIO()
+    canvas = FigureCanvas(fig)
+    canvas.print_png(png_image)
+
+    # Encode PNG image to base64 string
+    png_image_64 = "data:image/png;base64,"
+    png_image_64 += base64.b64encode(png_image.getvalue()).decode('utf8')
+
+    # Pass the BytesIO object to the template
+    return render_template('graph.html', image=png_image_64)
+
+@app.route('/boxPlot', methods=['GET', 'POST'])
+def boxPlot():
+    # Fetch data from the database
+    con = sqlite3.connect("github_trending.db")
+    cur = con.cursor()
+    cur.execute("SELECT language, COUNT(*) FROM repositories GROUP BY language")
+    data = cur.fetchall()
+    con.close()
+
+    # Convert the data to a DataFrame
+    df = pd.DataFrame(data, columns=['language', 'count'])
+
+    # Create a Matplotlib figure
+    fig = plt.figure(figsize=(10, 6))
+
+    # Use the DataFrame for plotting
+    sns.boxplot(x='language', y='count', data=df)
+    plt.xlabel('Programming Language')
+    plt.ylabel('Stars')
+    plt.title('Stars Distribution by Language')
+    # Rotate x-axis labels for better readability
+    plt.xticks(rotation=45, ha='right')
+    # Save the plot to a BytesIO object
+    png_image = io.BytesIO()
+    canvas = FigureCanvas(fig)
+    canvas.print_png(png_image)
+
+    # Encode PNG image to base64 string
+    png_image_64 = "data:image/png;base64,"
+    png_image_64 += base64.b64encode(png_image.getvalue()).decode('utf8')
+
+    # Pass the BytesIO object to the template
+    return render_template('graph.html', image=png_image_64)
 
 if __name__ == '__main__':
     app.run(debug=True)
